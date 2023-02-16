7549b63c36858c777b884a0bab4b3da3c8a636eb
Rea <andrea.solisg@gmail.com>
2023-02-16 13:49:22 +0100

    DELETE gossip added plus city's show with all gossips

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,2 +1,7 @@
 class CitiesController < ApplicationController
+    def show
+        @city = City.find((params[:id]).to_i)
+        @users_in_city = User.where(city_id: @city.id)
+        @gossips_in_city = Gossip.where(user: @users_in_city)
+    end
 end
